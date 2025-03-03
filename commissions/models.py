6aa3edb9451d73d64bb0c8c78c16ead457193009
evil levi <127681855+TheIronicHoodies@users.noreply.github.com>
2025-03-03 22:43:31 +0800

    Created models

--- commissions/models.py
+++ commissions/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Commission(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=False)
+    people_reqd = models.IntegerField(null=False)
+    created_on = models.DateTimeField() # incomplete
+    last_updated = models.DateTimeField() # incomplete
+
+class Comment(models.Model):
+    commission = models.ForeignKey(Commission, null=False, on_delete=models.CASCADE)
+    entry = models.TextField(blank=False)
+    created_on = models.DateTimeField() # incomplete
+    last_updated = models.DateTimeField() # incomplete
