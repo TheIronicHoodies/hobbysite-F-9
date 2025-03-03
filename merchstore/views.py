a2080469086a945928b94029ee1094c2796185ec
XanderAlmario <christian.almario@student.ateneo.edu>
2025-03-03 23:23:25 +0800

    Updated views.py to implement list and detail view

--- merchstore/views.py
+++ merchstore/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .models import Product
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 # Create your views here.
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'product_list.html'
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product_detail.html'
