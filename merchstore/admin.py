0ce447e79fab38a671408f98f98bc92d641b038c
XanderAlmario <christian.almario@student.ateneo.edu>
2025-03-04 00:02:17 +0800

    Updated admin.py to create an admin panel

--- merchstore/admin.py
+++ merchstore/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Product, ProductType
 # Register your models here.
+
+class ProductInLine(admin.TabularInline):
+    model = Product
+
+class ProductTypeAdmin(admin.ModelAdmin):
+    inlines = [ProductInLine,]
+    model = ProductType
+
+admin.site.register(ProductType, ProductTypeAdmin)
