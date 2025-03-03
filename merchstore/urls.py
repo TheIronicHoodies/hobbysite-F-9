7ec19abe9b018522f163a927250c6df46acdcfc5
XanderAlmario <christian.almario@student.ateneo.edu>
2025-03-04 01:30:38 +0800

    Changed the path links in urls.py for the merchstore app

--- merchstore/urls.py
+++ merchstore/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import ProductListView, ProductDetailView
 
 urlpatterns = [
-    path('merchstore/items', ProductListView.as_view(), name='product-list'),
-    path('merchstore/item/<int:pk>', ProductDetailView.as_view(), name='product')
+    path('/items', ProductListView.as_view(), name='product-list'),
+    path('/item/<int:pk>', ProductDetailView.as_view(), name='product')
 ]
 
 app_name = "merchstore"
