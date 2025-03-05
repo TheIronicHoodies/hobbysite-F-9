da018faa42acae0853a844c53d923357c870f6bb
Jerard Cruz <jerard-paolo.cruz@student.ateneo.edu>
2025-03-05 21:00:55 +0800

    Updated urls.py to create a path to the list view and detail view

--- forum/urls.py
+++ forum/urls.py
@@ -1,3 +1,10 @@
 from django.urls import path
 from .views import PostListView, PostDetailView
 
+urlpatterns = [
+    path("threads/", PostListView.as_view(), name="thread-list"),
+    path("thread/<int:pk/", PostDetailView.as_view(), name="thread-detail"),
+]
+
+app_name = "forum"
+
