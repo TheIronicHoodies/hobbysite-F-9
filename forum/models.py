7c130db3d060e2025df40336f2d39f6409bd47e2
Jerard Cruz <jerard-paolo.cruz@student.ateneo.edu>
2025-03-05 21:51:55 +0800

    Changed views.py to implement meta classes to sort posts

--- forum/models.py
+++ forum/models.py
@@ -6,6 +6,9 @@ class PostCategory(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
 
+    class Meta:
+        ordering = ["name"] 
+
     def __str__(self):
         return self.name
 
@@ -15,6 +18,9 @@ class Post(models.Model):
     entry = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"] 
 
     def __str__(self):
         return self.title
