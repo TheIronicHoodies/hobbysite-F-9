3e10d0a08c1c20043562bc549d9e0a52cb3854bb
Jerard Cruz <jerard-paolo.cruz@student.ateneo.edu>
2025-03-05 21:36:31 +0800

    Updated views.py to sort posts by order

--- forum/views.py
+++ forum/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from .models import Post
 
 def post_list(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('-created_on')  
     return render(request, "forum/post_list.html", {"posts": posts})
 
 def post_detail(request):
-    post = Post.objects.all() 
+    post = Post.objects.all()
     return render(request, "forum/post_detail.html", {"post": post})
 
