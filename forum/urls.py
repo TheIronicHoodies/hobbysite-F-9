ed66db5f36d27777e7e521fd441f536d59cbd911
Jerard Cruz <jerard-paolo.cruz@student.ateneo.edu>
2025-03-05 21:23:45 +0800

    Updated views.py to return list view and detail view

--- forum/urls.py
+++ forum/urls.py
@@ -3,8 +3,7 @@ from .views import PostListView, PostDetailView
 
 urlpatterns = [
     path("threads/", PostListView.as_view(), name="thread-list"),
-    path("thread/<int:pk/", PostDetailView.as_view(), name="thread-detail"),
+    path("thread/1/", PostDetailView.as_view(), name="thread-detail"),
 ]
 
 app_name = "forum"
-
--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, "forum/post_list.html", {"posts": posts})
+
+def post_detail(request):
+    post = Post.objects.all() 
+    return render(request, "forum/post_detail.html", {"post": post})
+
