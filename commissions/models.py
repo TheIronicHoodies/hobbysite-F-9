ba0bb42ed999e14e6d41dd388c32d9eb6a2b3b7e
evil levi <127681855+TheIronicHoodies@users.noreply.github.com>
2025-03-06 16:01:44 +0800

    finished commissions; added admin

--- commissions/models.py
+++ commissions/models.py
@@ -3,12 +3,18 @@ from django.db import models
 class Commission(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=False)
-    people_reqd = models.IntegerField(null=False)
-    created_on = models.DateTimeField() # incomplete
-    last_updated = models.DateTimeField() # incomplete
+    people_required = models.IntegerField(null=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_on',]
 
 class Comment(models.Model):
     commission = models.ForeignKey(Commission, null=False, on_delete=models.CASCADE)
     entry = models.TextField(blank=False)
-    created_on = models.DateTimeField() # incomplete
-    last_updated = models.DateTimeField() # incomplete
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_on',]
