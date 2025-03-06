a64eff280873c7147247ea450ad438cdeb972ce0
furudooru <160915885+furudooru@users.noreply.github.com>
2025-03-06 20:13:46 +0800

    Updated views.py
    PEP 8 compliance change

--- wiki/views.py
+++ wiki/views.py
@@ -5,7 +5,8 @@ from .models import Article, ArticleCategory
 class ArticleListView(ListView):
     model = ArticleCategory
     template_name = 'wiki_list.html'
-    
+
+
 class ArticleDetailView(DetailView):
     model = Article
     template_name = 'wiki_detail.html'
