ba04b6db70ed235c92e4e1364d6a2ccec83a36d0
Jerard Cruz <jerard-paolo.cruz@student.ateneo.edu>
2025-03-06 19:04:17 +0800

    Fixed urls.py and views.py

--- forum/urls.py
+++ forum/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import post_list, post_detail
+from .views import forum_list, forum_detail
 
 urlpatterns = [
-    path("threads/", post_list, name="thread-list"),
-    path("thread/<int:pk>/", post_detail, name="thread-detail"),
+    path("threads/", forum_list, name="thread-list"),
+    path("thread/<int:pk>/", forum_detail, name="thread-detail"),
 ]
 
 app_name = "forum"
--- forum/views.py
+++ forum/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from .models import Post
 
-def post_list(request):
+def forum_list(request):
     posts = Post.objects.all()
-    return render(request, "forum/post_list.html", {"posts": posts})
+    return render(request, "forum/forum_list.html", {"posts": posts})
 
-def post_detail(request, pk):
+def forum_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, "forum/post_detail.html", {"post": post})
+    return render(request, "forum/forum_detail.html", {"post": post})
 
