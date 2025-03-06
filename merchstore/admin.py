65cc82bbb17e64784d4387f2f45aff23bae31ab7
XanderAlmario <christian.almario@student.ateneo.edu>
2025-03-06 20:13:35 +0800

    Revert "Updated admin.py to create an admin panel"
    This reverts commit 0ce447e79fab38a671408f98f98bc92d641b038c.

--- merchstore/admin.py
+++ merchstore/admin.py
@@ -1,12 +1,3 @@
 from django.contrib import admin
-from .models import Product, ProductType
+
 # Register your models here.
-
-class ProductInLine(admin.TabularInline):
-    model = Product
-
-class ProductTypeAdmin(admin.ModelAdmin):
-    inlines = [ProductInLine,]
-    model = ProductType
-
-admin.site.register(ProductType, ProductTypeAdmin)
