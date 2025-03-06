930820b764d4b2110cef89652887b2c3bb482773
XanderAlmario <christian.almario@student.ateneo.edu>
2025-03-06 20:14:10 +0800

    Revert "Updated urls.py to create a path to the list view and detail view of the merchstore app"
    This reverts commit 443b5083edabe10964129b3a43623ff3391dd8a8.

--- merchstore/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import ProductListView, ProductDetailView
-
-urlpatterns = [
-    path('merchstore/items', ProductListView.as_view(), name='product-list'),
-    path('merchstore/item/<int:pk>', ProductDetailView.as_view(), name='product')
-]
-
-app_name = "merchstore"
