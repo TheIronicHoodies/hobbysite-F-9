b4740f5590e7963b9c7c79d084c1aa884a8e7d47
XanderAlmario <christian.almario@student.ateneo.edu>
2025-03-06 23:33:32 +0800

    Created the ProductType and Product models according to the specs

--- merchstore/models.py
+++ merchstore/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class ProductType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('merchstore:productType', args=[str(self.pk)])
+    
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    productType = models.ForeignKey (
+        ProductType,
+        on_delete=models.SET_NULL,
+        related_name='product'
+    )
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('merchstore:product', args=[str(self.pk)])
