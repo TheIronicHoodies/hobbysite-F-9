5a9dd4521590b32207f89dc3ae16ddc9b807d6ab
XanderAlmario <christian.almario@student.ateneo.edu>
2025-03-06 20:14:14 +0800

    Revert "Updated views.py to implement list and detail view"
    This reverts commit a2080469086a945928b94029ee1094c2796185ec.

--- merchstore/views.py
+++ merchstore/views.py
@@ -1,13 +1,3 @@
-from .models import Product
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
+from django.shortcuts import render
 
 # Create your views here.
-
-class ProductListView(ListView):
-    model = Product
-    template_name = 'product_list.html'
-
-class ProductDetailView(DetailView):
-    model = Product
-    template_name = 'product_detail.html'
