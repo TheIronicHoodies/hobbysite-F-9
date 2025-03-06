339dda2f235c5c513f3dbc7581c1368d5425e630
evil levi <127681855+TheIronicHoodies@users.noreply.github.com>
2025-03-06 22:15:12 +0800

    added str, absolute url to models

--- commissions/models.py
+++ commissions/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Commission(models.Model):
     title = models.CharField(max_length=255)
@@ -10,11 +11,20 @@ class Commission(models.Model):
     class Meta:
         ordering = ['created_on',]
 
+    def __str__(self):
+        return str(self.title)
+    
+    def get_absolute_url(self):
+        return reverse('commission:commissions-detail', args=[str(self.id)])
+
 class Comment(models.Model):
-    commission = models.ForeignKey(Commission, null=False, on_delete=models.CASCADE)
+    commission = models.ForeignKey(Commission, null=False, on_delete=models.CASCADE, related_name='comment')
     entry = models.TextField(blank=False)
     created_on = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ['-created_on',]
+
+    def __str__(self):
+        return str(self.entry)
