f4993caaac48a66d3e4d83c1a134511b0eb846fe
XanderAlmario <christian.almario@student.ateneo.edu>
2025-03-06 20:13:42 +0800

    Revert "Updated models.py with __str__ and get_absolute_url methods for each model"
    This reverts commit 992ce4a8e62ac3f698fed7cd2549f2ae4073d2c7.

--- merchstore/models.py
+++ merchstore/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.urls import reverse
 
 # Create your models here.
 class ProductType(models.Model):
@@ -7,29 +6,17 @@ class ProductType(models.Model):
     description = models.TextField
 
     class Meta:
-        ordering = ['name']
-    
-    def __str__(self):
-        return self.name
-    
-    def get_absolute_url(self):
-        return reverse('merchstore:productType', args=[str(self.name)])
+            ordering = ['name']
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     productType = models.ForeignKey(
         ProductType,
         on_delete=models.CASCADE,
-        related_name='productType'
+        related_name='ProductType'
     )
     description = models.TextField
     price = models.DecimalField(decimal_places=2)
     
     class Meta:
-        ordering = ['name']
-
-    def __str__(self):
-        return self.name
-    
-    def get_absolute_url(self):
-        return reverse('merchstore:product', args=[str(self.name)])
+            ordering = ['name']
