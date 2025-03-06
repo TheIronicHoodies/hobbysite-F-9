5bc01259930b220918fdc402b41cb22b2a1dc393
Jerard Cruz <jerard-paolo.cruz@student.ateneo.edu>
2025-03-06 17:05:15 +0800

    Edited admin.py to create a admin panel

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import PostCategory, Post
 
-# Register your models here.
+class PostCategoryAdmin(admin.ModelAdmin):
+    search_fields = ("name",)  
+    list_display = ("name", "description")  
+    ordering = ("name",) 
+
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ("title", "entry") 
+    list_display = ("title", "category", "created_on", "updated_on")  
+    list_filter = ("category",)  
+    ordering = ("-created_on",)  
+
+admin.site.register(PostCategory, PostCategoryAdmin)
