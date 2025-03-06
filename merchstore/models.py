c76550d8537ff66924aebee0b03a46b1c693c203
XanderAlmario <christian.almario@student.ateneo.edu>
2025-03-06 20:14:18 +0800

    Revert "Updated models.py by adding all the specified models in the specs"
    This reverts commit 7f58b8bb913c52b5814c98475971c51879de38da.

--- merchstore/models.py
+++ merchstore/models.py
@@ -1,16 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class ProductType(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField
-
-class Product(models.Model):
-    name = models.CharField(max_length=255)
-    productType = models.ForeignKey(
-        ProductType,
-        on_delete=models.CASCADE,
-        related_name='ProductType'
-    )
-    description = models.TextField
-    price = models.DecimalField(decimal_places=2)
