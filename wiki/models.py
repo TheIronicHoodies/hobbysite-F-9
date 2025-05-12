4c366f0e4cb002397e80bb8df5685d2c7fb70460
furudooru <160915885+furudooru@users.noreply.github.com>
2025-05-12 18:52:22 +0800

    Updated wiki functionality
    updated wiki to function as desired by project specs

--- wiki/models.py
+++ wiki/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from user_management.models import Profile
 
 
 class ArticleCategory(models.Model):
@@ -16,10 +17,13 @@ class ArticleCategory(models.Model):
     def __str__(self):
         return f'{self.name}'
 
+
 class Article(models.Model):
     title = models.CharField(max_length=255)
-    category = models.ForeignKey(ArticleCategory, on_delete=models.SET_NULL, null=True, related_name='articles')
+    category = models.ForeignKey(ArticleCategory, on_delete=models.SET_NULL, null=True, related_name='articles',)
+    author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True) #we will work this out
     entry = models.TextField()
+    header_image = models.ImageField(null=True, upload_to='images/') #not too much of an issue
     created_on = models.DateTimeField(auto_now_add=True, null=False)
     updated_on = models.DateTimeField(auto_now=True)
     
@@ -32,3 +36,22 @@ class Article(models.Model):
     
     def __str__(self):
         return f'{self.title}'
+    
+    
+class Comment(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True) #we will work this out
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, null=True, related_name='comments',)
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True, null=False)
+    updated_on = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ["created_on"]
+        
+    def get_absolute_url(self):
+        return reverse('wiki:article', args=[self.article.pk])
+    
+    def __str__(self):
+        return f'{self.article}: {self.entry}'
+    
+
--- wiki/urls.py
+++ wiki/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import ArticleListView, ArticleDetailView
+from .views import ArticleListView, ArticleDetailView, ArticleCreateView, ArticleUpdateView
 
 urlpatterns = [
     path('articles/list', ArticleListView.as_view(), name='list'),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article'),
+    path('article/add', ArticleCreateView.as_view(), name='create'),
+    path('article/<int:pk>/edit', ArticleUpdateView.as_view(), name='update'),
 ]
 
 app_name = "wiki"
