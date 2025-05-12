d1dbe0f5bceb6c9a1886ad07ae81d854d8327608
XanderAlmario <christian.almario@student.ateneo.edu>
2025-05-12 17:36:50 +0800

    Added paths to the new vies

--- merchstore/urls.py
+++ merchstore/urls.py
@@ -1,11 +1,19 @@
 """Sets paths to merchstore's list and detail view."""
 
 from django.urls import path
-from .views import ProductListView, ProductDetailView
+from .views import( 
+    ProductListView, ProductDetailView, ProductCreateView, 
+    ProductUpdateView, CartView, TransactionsListView
+)
+
 
 urlpatterns = [
     path('merchstore/items', ProductListView.as_view(), name='product-list'),
-    path('merchstore/item/<int:pk>', ProductDetailView.as_view(), name='product')
+    path('merchstore/item/<int:pk>', ProductDetailView.as_view(), name='product'),
+    path('merchstore/item/add', ProductCreateView.as_view(), name='add-product'),
+    path('merchstore/item/<int:pk>/edit', ProductUpdateView.as_view(), name='product-update'),
+    path('merchstore/cart', CartView.as_view(), name='cart'), 
+    path('merchstore/transactions', TransactionsListView.as_view(), name='transactions')
 ]
 
 app_name = "merchstore"
