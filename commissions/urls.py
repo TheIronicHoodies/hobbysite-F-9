399c859a73ff94299b0be29c498dd69173bb115c
evil levi <127681855+TheIronicHoodies@users.noreply.github.com>
2025-05-14 22:36:33 +0800

    started commissions create and upload, minor debugging on list

--- commissions/urls.py
+++ commissions/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import CommissionsDetail, CommissionsList
+from .views import *
 
 urlpatterns = [
     path('list', CommissionsList.as_view(), name='commissions-list'),
-    path('detail/<int:pk>', CommissionsDetail.as_view(), name='commissions-detail')
+    path('detail/<int:pk>', CommissionsDetail.as_view(), name='commissions-detail'),
+    path('add', CommissionsCreate.as_view(), name='commissions-add'),
+    path('<int:pk>/edit', CommissionsUpdate.as_view(), name='commissions-edit')
 ]
 
 app_name = 'commissions'
