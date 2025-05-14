7edaebde93a423fd6ce815f7f683a3a37ee00a21
Jerard Cruz <jerard-paolo.cruz@student.ateneo.edu>
2025-05-14 20:34:34 +0800

    Fixed models.py to import Profile from user_management

--- forum/models.py
+++ forum/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-from hobbysite.models import Profile
+from user_management.models import Profile
 
 
 class ThreadCategory(models.Model):
