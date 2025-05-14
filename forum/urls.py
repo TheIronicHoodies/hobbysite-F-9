9068ede37abf1d6a25dedeb492547489faf7adf2
Jerard Cruz <jerard-paolo.cruz@student.ateneo.edu>
2025-05-14 21:04:29 +0800

    Updated urls.py to match views.py

--- forum/urls.py
+++ forum/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import ThreadDetailView, ThreadListView, ThreadCreateView, ThreadUpdateView
 
 urlpatterns = [
-    path("threads/", PostListView.as_view(), name="thread-list"),
-    path("thread/<int:pk>/", PostDetailView.as_view(), name="thread-detail"),
+    path("threads/", ThreadListView.as_view(), name="thread-list"),
+    path("thread/<int:pk>/", ThreadDetailView.as_view(), name="thread-detail"),
+    path("thread/add/", ThreadCreateView.as_view(), name="add-thread"),
+    path("thread/<int:pk>/edit/", ThreadUpdateView.as_view(), name="edit-thread"),
 ]
 
 app_name = "forum"
