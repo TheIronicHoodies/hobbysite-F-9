21a7f1e36e05433ef3e31a64c70f1fc4c9805696
Jerard Cruz <jerard-paolo.cruz@student.ateneo.edu>
2025-05-15 21:46:42 +0800

    Fixed urls.py

--- forum/urls.py
+++ forum/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import ThreadDetailView, ThreadListView, ThreadCreateView, ThreadUpdateView
 
 urlpatterns = [
-    path("threads/", ThreadListView.as_view(), name="thread-list"),
-    path("thread/<int:pk>/", ThreadDetailView.as_view(), name="thread-detail"),
-    path("thread/add/", ThreadCreateView.as_view(), name="add-thread"),
-    path("thread/<int:pk>/edit/", ThreadUpdateView.as_view(), name="edit-thread"),
+    path("threads/", ThreadListView.as_view(), name="thread_list"),
+    path("thread/<int:pk>/", ThreadDetailView.as_view(), name="thread_detail"),
+    path("thread/add/", ThreadCreateView.as_view(), name="add_thread"),
+    path("thread/<int:pk>/edit/", ThreadUpdateView.as_view(), name="edit_thread"),
 ]
 
 app_name = "forum"
