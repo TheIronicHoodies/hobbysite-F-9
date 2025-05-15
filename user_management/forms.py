9935d4171c2eef93b180ba2e7203304ddf330186
evil levi <127681855+TheIronicHoodies@users.noreply.github.com>
2025-05-15 18:00:48 +0800

    tried to fix profile forms

--- user_management/forms.py
+++ user_management/forms.py
@@ -1,14 +1,24 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Profile
 
 
-class ProfileCreateForm(forms.ModelForm):
+class ProfileCreateForm(UserCreationForm):
+    name = forms.CharField(max_length=24)
+    email_address = forms.EmailField()
+
     class Meta:
-        model = Profile
-        fields = '__all__'
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'password1','password2']
 
 
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ["name", "email_address"]
+        fields = ["name", "email_address",]
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
