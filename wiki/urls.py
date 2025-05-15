bfea28f50a87a188b20b4bf48f4bcb96eb12fe5f
furudooru <160915885+furudooru@users.noreply.github.com>
2025-05-15 21:02:52 +0800

    Updated urls.py to match specs

--- wiki/urls.py
+++ wiki/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import ArticleListView, ArticleDetailView, ArticleCreateView, ArticleUpdateView, ImageCreateView
 
 urlpatterns = [
-    path('articles/list', ArticleListView.as_view(), name='list'),
+    path('articles', ArticleListView.as_view(), name='list'),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article'),
     path('article/add', ArticleCreateView.as_view(), name='create'),
     path('article/<int:pk>/edit', ArticleUpdateView.as_view(), name='update'),
