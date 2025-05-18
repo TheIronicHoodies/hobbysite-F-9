f02d2f9a2c040267b3b573302724e71ae6bece04
evil levi <127681855+TheIronicHoodies@users.noreply.github.com>
2025-05-19 03:00:04 +0800

    minor change in forum

--- forum/urls.py
+++ forum/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from .views import PostListView, PostDetailView
 
 urlpatterns = [
-    path("threads/", PostListView.as_view(), name="thread-list"),
+    path("threads/", PostListView.as_view(), name='thread-list'),
     path("thread/<int:pk>/", PostDetailView.as_view(), name="thread-detail"),
 ]
 
--- home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import home_list
-
-
-urlpatterns = [
-    path('', home_list, name='home')
-]
-
-app_name = "home"
