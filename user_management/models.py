da86974e0d23ac40be70b365622cc4fab6369af6
evil levi <127681855+TheIronicHoodies@users.noreply.github.com>
2025-05-20 01:05:44 +0800

    saving

--- user_management/models.py
+++ user_management/models.py
@@ -5,8 +5,8 @@ from django.urls import reverse #filler
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,)
-    # name = models.CharField(max_length=63)
-    # email_address = models.EmailField()
+    name = models.CharField(max_length=63)
+    email_address = models.EmailField()
 
     def __str__(self):
         return f'{self.user.first_name}'
