b397807b7d4f350b70e1118d0bc5c0ac6a5fb5cf
evil levi <127681855+TheIronicHoodies@users.noreply.github.com>
2025-05-20 01:18:47 +0800

    made registration work

--- user_management/urls.py
+++ user_management/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ProfileCreate, ProfileUpdate
 
 urlpatterns = [
-    path('register', ProfileCreate.as_view(), name='register'),
+    path('register', ProfileCreate.as_view(success_url="/accounts/login"), name='register'),
     path('<str:username>', ProfileUpdate.as_view(), name='update'),
 ]
 
